01f5ee6f09e037bbec56233571522d44bc1d5cc0
Jonas Westerlund <jonas.westerlund@helsinki.fi>
2017-08-19 14:04:04 +0300

    Update

--- src/main/java/wad/MultiEmitApplication.java
+++ src/main/java/wad/MultiEmitApplication.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package wad;
 
-/**
- *
- * @author Jonas
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MultiEmitApplication {
+    
+    public static void main(String[] args){
+        SpringApplication.run(MultiEmitApplication.class, args);
+    }
     
 }
--- src/main/java/wad/service/CustomUserDetailsService.java
+++ src/main/java/wad/service/CustomUserDetailsService.java
@@ -7,10 +7,11 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import wad.domain.UserAccount;
 import wad.repository.UserAccountRepository;
 
-
+@Service
 public class CustomUserDetailsService implements UserDetailsService{
 
     @Autowired
